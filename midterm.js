489307aa0b0f4282dff813c50e5f52a0bc730ff2
Rafael Vargas <rafael.vargas@outlook.com>
2021-06-17 10:46:49 -0400

    add div, main, and target

--- midterm.js
+++ midterm.js
@@ -1,9 +1,12 @@
-// Create DIV element on page
-
+// Create DIV element to place on page
 var div = document.createElement('div')
 
 // Add class to newly created DIV
 div.classList.add('div-content')
+
+// Create targert and place inside
+var target = document.querySelector('main')
+target.prepend(div)
 
 // TODO: Create querySlector
 var clicker = document.querySelector('body')
