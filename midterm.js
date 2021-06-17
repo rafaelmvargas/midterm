52fffa39bc8ece2618304e87c3ceb16812144203
Rafael Vargas <rafael.vargas@outlook.com>
2021-06-17 13:12:36 -0400

    something akin to an event listener

--- midterm.js
+++ midterm.js
@@ -4,12 +4,29 @@ var div = document.createElement('div')
 // Add class to newly created DIV
 div.classList.add('div-content')
 
-// Create targert and place inside
+// Get a targert and place inside
 var target = document.querySelector('main')
-target.prepend(div)
+target.append(div)
 
-// TODO: Create querySlector
-var clicker = document.querySelector('body')
+function menuClicked(event) {
+  console.log(document.location.pathname)
+  var tabSelected = (document.querySelector(
+    'body'
+  ).output = `<h1>You clicked: ${tabSelected} tab</h1>`)
+
+  if (event.target.matches('.nav a')) {
+    div.innerHTML = output
+    //console.log(output)
+  } else {
+    return
+  }
+  event.preventDefault()
+}
+
+document.addEventListener('click', menuClicked)
+
+// var tabSelected = window.location
+// setup template literal
 
 // TODO: Add event listener to the page using event delegation
 // clicker.addEventListener('click', whatMenuWasSelected)
