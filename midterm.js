574046c40f231fb93a26d2e498548008080ecb9e
Rafael Vargas <rafael.vargas@outlook.com>
2021-06-18 18:10:23 -0400

    renamed popover to div-content to make it generic

--- midterm.js
+++ midterm.js
@@ -2,30 +2,37 @@
 var popOver = document.createElement('div')
 
 // Add class to newly created DIV
-popOver.classList.add('beta-info')
+div.classList.add('div-content')
 
 // Get a targert and place inside
 var target = document.querySelector('main')
-target.append(popOver)
+target.append(div)
 
 function clickHandler(event) {
   console.log(event.target)
 
   var eventTargetHash = event.target.hash
   if (eventTargetHash) {
+    navRemoveActiveAll()
+
     console.log(eventTargetHash)
-    popOver.innerHTML = eventTargetHash
+    div.innerHTML = eventTargetHash
   }
 
   if (event.target.matches('.sign-out-link')) {
-    popOver.innerHTML = 'Singing out, please wait'
+    div.innerHTML = 'Singing out, please wait'
   }
 
   event.preventDefault()
 }
 
-function popOver(message) {
-  popOver.innerHTML = `${message}`
+function navRemoveActiveAll() {
+  var tabs = document.querySelector('na ul li a')
+  tabs.classList.remove('.active')
+}
+
+function div(message) {
+  div.innerHTML = `${message}`
 }
 
 document.addEventListener('click', clickHandler, false)
